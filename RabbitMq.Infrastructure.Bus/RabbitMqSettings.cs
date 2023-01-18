959bcce18ae0e04e0238551e1a7f3c4a0ecf427c
BentoAldo <bentoaldo@hotmail.com>
2023-01-17 21:16:01 -0300

    chore: deleted unused projects

--- RabbitMq.Infrastructure.Bus/RabbitMqSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace RabbitMq.Infrastructure.Bus;
-
-public class RabbitMqSettings
-{
-    public string HostName { get; set; } = string.Empty;
-    public string UserName { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-}
