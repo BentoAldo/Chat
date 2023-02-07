f76d789a6a0277937930b2f60bc9b559cab9b9a9
Aldo Bento <aldobento@greenlight.com.py>
2023-02-07 16:28:25 -0300

    Update StockServicesTests.cs

--- StockBot.Tests/StockServicesTests.cs
+++ StockBot.Tests/StockServicesTests.cs
@@ -18,14 +18,34 @@ public class StockServicesTests
     public void GetStockAsync_InputCode_ReturnStock()
     {
         // Arrange
-        const string code = "AAPL";
+        const string code = "aapl.us";
 
         // Act
-        _stockServices.Setup(x => x.GetStockAsync(code)).ReturnsAsync(new BaseResponse<Stock>());
+        _stockServices.Setup(x => x.GetStockAsync(code)).ReturnsAsync(new BaseResponse<Stock>
+        {
+            Success = true,
+            Data = new Stock
+            {
+                Symbol= code.ToUpper(),
+                Date = DateOnly.FromDateTime(DateTime.Now),
+                Open = "100",
+                High = "200",
+                Low = "50",
+                Close = "150",
+                Volume = 1000
+            }
+        });
+        
         var actual = _stockServices.Object.GetStockAsync(code).Result;
 
         // Assert
-        Assert.That(actual, Is.Not.Null);
-        Assert.That(actual, Is.TypeOf<BaseResponse<Stock>>());
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual, Is.Not.Null);
+            Assert.That(actual, Is.TypeOf<BaseResponse<Stock>>());
+            Assert.That(actual.Success, Is.True);
+            Assert.That(actual.Data, Is.Not.Null);
+            Assert.That(actual.ProblemDetails, Is.Null);
+        });
     }
 }
